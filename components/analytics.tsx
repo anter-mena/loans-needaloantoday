1639548160e839ebc7d8d5f838258723cd7a1b21
AnterMena <antermena.studio@gmail.com>
2026-08-13 15:08:29 +0100

    feat: add Microsoft Clarity tracking script to layout

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -55,6 +55,17 @@ export default function Analytics() {
           });
         `}
       </Script>
+
+      {/* Microsoft Clarity */}
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "y1rmb88ote");
+        `}
+      </Script>
     </>
   );
 }
